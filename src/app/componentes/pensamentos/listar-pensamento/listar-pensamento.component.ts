72bcbbdd11b0f030a4f93131297543bd656b8701
Thiago Andrade <imthiagomartins@live.com>
2024-06-23 14:26:55 -0300

    Iniciando comunicação com backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,23 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit{
 
-  listaPensamentos = [
-    {
-      conteudo: 'TESTE TESTE',
-      autoria: 'Autor',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'TESTE TESTE',
-      autoria: 'Autor',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'TESTE TES laksdgfdas as d4a56s4 654as65d4 65as465d4 a65s4 65as4d65a4s 654as6 54d65as4 56d465as 456as4d65as4 654d6 5a4s65d4 6as54d 65as4654d a6s54 6d5as46 5d4as65d4 as65ksdhg ahjfgsadhjkfgjks hdghjksdgfjkh sagjhkf gsdjkhag jkhsdg jhsgdjh gsadjkgfjkhsdagjhkfsdgfhsdgfhjksdgjkfhgsdjkgshjd gjkfsadTE',
-      autoria: 'Autor',
-      modelo: 'modelo3'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() {}
   
