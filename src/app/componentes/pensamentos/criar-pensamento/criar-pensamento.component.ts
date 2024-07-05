4e914e4e1cb426bdaaf8db6ce489cfa9c7f1deb6
Thiago Andrade <imthiagomartins@live.com>
2024-07-04 22:22:27 -0300

    Adicionando validações nos forms reativos

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,7 +2,7 @@ import { Component, ModuleWithComponentFactories } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -21,19 +21,37 @@ export class CriarPensamentoComponent {
 
   ngOnInit(): void {  
     this.formulario = this.formBuilder.group({
-      conteudo: ['Formulário reativo'],
-      autoria: ['Autoria'],
+      conteudo: ['', Validators.compose([
+        Validators.required, 
+        Validators.pattern(/(.|\s)*\S(.|\s)*/), 
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+      ])],
       modelo: ['modelo1']
     })
   }
 
   criarPensamento() {
-    this.service.criar(this.formulario.value).subscribe(()=> {
-      this.router.navigate(['/listarPensamento'])
-    })
+    console.log(this.formulario.get('autoria')?.errors)
+    if(this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe(()=> {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
 
   cancelarPensamento() {
     this.router.navigate(['/listarPensamento'])
   }
+
+  habilitarBotao(): string {
+    if(this.formulario.valid) {
+      return 'botao'
+    } else {
+      return 'botao__desabilitado'
+    }
+  }
 }
+
